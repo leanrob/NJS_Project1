10313edf117df99712ae24265f910b82d9ba08df
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-24 02:53:09 -0230

    Using the Node event emitter and naming convention
    This commit is the first using the Node event emitter in app.js
    
    It also has a file named config.js which is all about having a naming convention inside a file and not relying on spelling 'magig strings' correctly.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var Emitter = require('./emitter');
+var Emitter = require('events');
+
+var eventConfig = require('./config').events;
 
 var emtr = new Emitter();
 
 // adding a listener function on the greet method
-emtr.on('greet', function() {
-    console.log("Someone, somewhere sai hello");
+emtr.on(eventConfig.GREET, function() {
+    console.log("Someone, somewhere say hello");
 });
 
 // adding another sure why not
-emtr.on('greet', function() {
+emtr.on(eventConfig.GREET, function() {
     console.log("There has been a greeting");
 });
 
 console.log('Hello!');
-emtr.emit('greet');
+emtr.emit(eventConfig.GREET);
