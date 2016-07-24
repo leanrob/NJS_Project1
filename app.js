6b824a83528a86f86afbac46c8077cc0c1dc8c75
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-24 03:34:56 -0230

    Utils.inherits and template literal syntax
    Utils.inherits and object literal syntax examples.
    
    template literals areonly available in ES6 and may not work on all browsers but will work in nodejs…
    
    Ce careful

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+// Inheriting from the event emitter...
+
+var EventEmitter = require('events');
+var util = require('util');
+var eventConfig = require('./config').events;
+
+function Greetr() {
+    this.greeting = 'Hello World';
+}
+
+// making sure that utils inherits is used to inherit events and our Greetr() function
+util.inherits(Greetr, EventEmitter);
+
+// The data in this can also be passed through the emitter 
+//  as long as we expect it down bellow when we add the listener
+Greetr.prototype.greet = function(data){
+    console.log(this.greeting + ': ' + data);
+    this.emit(eventConfig.GREET, data);
+}
+
+var greetr1 = new Greetr();
+
+// Now we can add an event emitter to our new Greetr() function namer greetr1
+// Every time the greet is run this event listener will be run
+greetr1.on(eventConfig.GREET, function(data) {
+    console.log('Someone Greeted!' + " " + data);
+});
+
+greetr1.greet('Robert');
+
+// Template literals ES6
+
+var name = "Robert";
+
+var greet = "hello " + name;
+
+// This is object Literal syntax
+var greet2 = `hello ${ name }`;
+
+console.log(greet);
+console.log(greet2);
+
+
+/* Previous code
+
 var Emitter = require('events');
 
 var eventConfig = require('./config').events;
@@ -16,3 +61,5 @@ emtr.on(eventConfig.GREET, function() {
 
 console.log('Hello!');
 emtr.emit(eventConfig.GREET);
+
+*/
