57dcca8a0c1093fedf1e6e41428b7fdb6c89037e
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-25 02:43:32 -0230

    First setup of a node webserver and making request
    Making simple request with the http-parser and running through this debugging to see how it worked.
    
    Make request to ‘localhost:1337’ once the app.js is run in terminal to make the “Hello world!” appear in the browser. This is something that came from my webserver and put it in the browser.
    
    This is the begining of real things to come in the course!

--- app.js
+++ app.js
@@ -1,117 +1,12 @@
-// Pipes and readable wrtiable
+// This is the basic setup of a node server, 
+//  and is made up of all the things learned up until now.
 
-var fs = require('fs');
-var zlib = require('zlib');
+var http = require('http');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt');
+// listening for a request on 1337 and do this nde code in the begining.
+http.createServer(function(req, res) {
 
-var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!\n');
 
-var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
-
-// Creating a compressed file as a stram and can pipe its output as well...
-var gzip = zlib.createGzip();
-
-// Simple way to send achunk of data from one file to anther through a pipe
-readable.pipe(writable);
-
-// on every chunk we will write to gzip and then write to compressed
-//  all of this is done with method chaining
-readable.pipe(gzip).pipe(compressed);
-
-
-/*
-
-// yncronous file read and this may not be the best.
-var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
-
-console.log(greet);
-
-// The last function is a callback, using Error First Callbacks
-var greet2 = fs.readFile(__dirname + '/greet.txt', 'utf8', function(err, data) {
-    console.log(data);
-});
-
-// This appears before greet2's callback because it was asyncronous
-console.log('Done!');
-
-// encoding added as an object in this data stream
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8' });
-
-readable.on('data', function(chunk) {
-    console.log(chunk);
-});
-
-*/
-/*
-
-// Inheriting from the event emitter...
-
-var EventEmitter = require('events');
-var util = require('util');
-var eventConfig = require('./config').events;
-
-function Greetr() {
-    // Adding a superconstructer for complete inheritance
-    // We do this because we want Greetr to be an event emitter 
-    //  and so one must be called and created whenever a Greetr is
-    EventEmitter.call(this);
-    this.greeting = 'Hello World';
-}
-
-// making sure that utils inherits is used to inherit events and our Greetr() function
-util.inherits(Greetr, EventEmitter);
-
-// The data in this can also be passed through the emitter 
-//  as long as we expect it down bellow when we add the listener
-Greetr.prototype.greet = function(data){
-    console.log(this.greeting + ': ' + data);
-    this.emit(eventConfig.GREET, data);
-}
-
-var greetr1 = new Greetr();
-
-// Now we can add an event emitter to our new Greetr() function namer greetr1
-// Every time the greet is run this event listener will be run
-greetr1.on(eventConfig.GREET, function(data) {
-    console.log('Someone Greeted!' + " " + data);
-});
-
-greetr1.greet('Robert');
-
-// Template literals ES6
-
-var name = "Robert";
-
-var greet = "hello " + name;
-
-// This is object Literal syntax
-var greet2 = `hello ${ name }`;
-
-console.log(greet);
-console.log(greet2);
-
-*/
-
-/* Previous code
-
-var Emitter = require('events');
-
-var eventConfig = require('./config').events;
-
-var emtr = new Emitter();
-
-// adding a listener function on the greet method
-emtr.on(eventConfig.GREET, function() {
-    console.log("Someone, somewhere say hello");
-});
-
-// adding another sure why not
-emtr.on(eventConfig.GREET, function() {
-    console.log("There has been a greeting");
-});
-
-console.log('Hello!');
-emtr.emit(eventConfig.GREET);
-
-*/
+}).listen(1337, '127.0.0.1');
