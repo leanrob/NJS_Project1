1b80f4e717d1f3aebc73bd537dcea96f7a8ba275
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-25 03:59:43 -0230

    Usinf streams and pipes to improve performance
    removing a fair amount of code and using pipe to do what we were just doing but..
    
    now we are doing it asyncronously.

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // sending the variable which is the contents of index.html
-    var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+
     // Using variables and replacing a Template setup in HTML
     // Replacing `{Message}` in index.html with message variable
     // This is forshadowing to Express I think
-    var message = 'Hello Moon!!!';
-    html = html.replace('{Message}', message);
-    res.end(html);
+    // var message = 'Hello Moon!!!';
+    // html = html.replace('{Message}', message);
+    // res.end(html);
 
 }).listen(1337, '127.0.0.1');
 
