e41df90716ac91472985e71688337a15d5f12a2e
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-25 03:50:43 -0230

    Sending HTML data over our webserver to 1337
    This code allows us to send the contents of index.html over our web server to the web browser.
    
    This means that the changes we make in our html will alwasy be fresh on the web server.
    
    It also shows the use of templates and how they can be replaced with variables in the Node/javascript side of things.
    
    All run fine.

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 //  and is made up of all the things learned up until now.
 
 var http = require('http');
+var fs = require('fs');
 
 // listening for a request on 1337 and do this nde code in the begining.
 http.createServer(function(req, res) {
+    // mimetype set to send htmls data
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
+    // sending the variable which is the contents of index.html
+    var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    // Using variables and replacing a Template setup in HTML
+    // Replacing `{Message}` in index.html with message variable
+    // This is forshadowing to Express I think
+    var message = 'Hello Moon!!!';
+    html = html.replace('{Message}', message);
+    res.end(html);
 
 }).listen(1337, '127.0.0.1');
+
+// How to run this:
+// run it in terminal, then make a browser request to 'localhost:1337'
