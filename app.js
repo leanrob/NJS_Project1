1cc374d664057017941bb06f3b762dcd82977327
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-27 01:44:56 -0230

    Showing routing in expressJS
    This code shows how we can use routing with variables in the url, those variables can then be accessed in the express functions run for that domain.

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', function(req, res) {
     res.send('<html><head></head><body><h1>Hello Guys!!!</h1></body></html>');
 });
 
+app.get('/person/:id', function(req, res) {
+    res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
 app.get('/api', function(req, res) {
     res.json({ firstname: 'Rob', lastname: 'Byrne'});
 });
