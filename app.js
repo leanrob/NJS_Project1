6508e5e4b58af2d8796c4d3ccd622a24d0015494
Robert Byrne <mr.rob.byrne@gmail.com>
2016-07-27 01:22:52 -0230

    Express, Nodemon, & NPM Init installed and used
    Express, Nodemon, & NPM Init installed and used.
    
    Expres is usd to make the same webserver code as before but a lot easier.
    
    Nodemon should be used from now on to auto reload the Node app.js when changes are made to it’s code.
    
    Init or packages.json in the folder coatains the dependancies for this code. `npm update` to update them.

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+// Much neater way to do what we did before, done this time with expressjs
+
+var express = require('express');
+var app = express();
+
+// Environment variable that we can use in the code
+var port = process.env.PORT || 3000;
+
+// Sending HTML data using express' get and send, only on '/'
+app.get('/', function(req, res) {
+    res.send('<html><head></head><body><h1>Hello Guys!!!</h1></body></html>');
+});
+
+app.get('/api', function(req, res) {
+    res.json({ firstname: 'Rob', lastname: 'Byrne'});
+});
+
+app.listen(port);
+
+/*
 // This is the basic setup of a node server, 
 //  and is made up of all the things learned up until now.
 
@@ -7,13 +27,22 @@ var fs = require('fs');
 // listening for a request on 1337 and do this nde code in the begining.
 http.createServer(function(req, res) {
     // mimetype set to send htmls data
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    // Transfering some JSON data through HTTP with the following code
 
-    var obj = {
-        firstname: 'John',
-        lastname: 'Deo'
-    };
-    res.end(JSON.stringify(obj));
+    if (req.url === '/') {
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+
+    if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        var obj = {
+            firstname: 'John',
+            lastname: 'Deo'
+        };
+        res.end(JSON.stringify(obj));
+    }
+
 
     // sending the variable which is the contents of index.html
     //fs.createReadStream(__dirname + '/index.html').pipe(res);
@@ -23,7 +52,7 @@ http.createServer(function(req, res) {
 
 // How to run this:
 // run it in terminal, then make a browser request to 'localhost:1337'
-
+*/
 /*
 // listening for a request on 1337 and do this nde code in the begining.
 http.createServer(function(req, res) {
